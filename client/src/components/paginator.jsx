751e66b627a38dcfa43e1c1c89d6462f7257bf0a
ChofGonzalez <titizapiola@gmail.com>
2022-08-15 12:12:14 -0300

    buttons next n prev

--- client/src/components/paginator.jsx
+++ client/src/components/paginator.jsx
@@ -1,6 +1,8 @@
+import React, { useState } from 'react'
 import styles from './css/paginator.module.css'
 
 export default function Paginado ({countriesPerPage, countries, paginado}){
+    const [currentPage, setCurrentPage] = useState(1)
     const numeroPaginas = [];
 
     for(let i = 1; i <= Math.ceil(countries / countriesPerPage); i++){
@@ -10,14 +12,22 @@ export default function Paginado ({countriesPerPage, countries, paginado}){
     return (
     <div className={styles.contenedor}>
         <ul className={styles.ul}>
-            {
-                numeroPaginas.map((numero) => (
-                    <li className={styles.li}>
-                        <button className={styles.btn} onClick={() => paginado(numero)}>{numero}</button>
-                    </li>
-                ))
-            }
+            {numeroPaginas.includes(currentPage +1) && <button className={styles.btn} onClick={()=>{
+            setCurrentPage(currentPage +1);
+            paginado(currentPage +1);
+        }}>NEXT</button>}
+            {numeroPaginas.map((numero) => (
+            <li className={styles.li}>
+            <button className={styles.btn} onClick={() => paginado(numero)}>{numero}</button>
+            </li>))}
+            {numeroPaginas.includes(currentPage -1) && <button className={styles.btn} onClick={()=>{
+            setCurrentPage(currentPage -1);
+            paginado(currentPage -1);
+        }}>PREV</button>}
         </ul>
     </div>
     )
 }
+
+
+
